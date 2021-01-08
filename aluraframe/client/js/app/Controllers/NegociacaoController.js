8b0b3028b77d52871085bc9f0fe8d770a68a9e67
Thiago Luz <thiagotls58@gmail.com>
2021-01-08 08:29:50 -0300

    Aula 03

---
+++ aluraframe/client/js/app/Controllers/NegociacaoController.js
@@ -0,0 +1,25 @@
+class NegociacaoController {
+
+    constructor() {
+        
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        //let data = new Date(this._inputData.value.split('-'));
+        let data = new Date(this._inputData.value.replace(/-/g, ','));
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        ); 
+
+        console.log(negociacao);
+    }
+}
